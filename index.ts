d2b86b682779833e0c7037f275c5ea703816a23c
Kenny Le <kenny.le@mths.ca>
2024-02-18 16:20:42 +0000

    init

---
+++ index.ts
@@ -0,0 +1,13 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * By:      Kenny Le
+ * Version: 1.0
+ * Since:   2024-02-16
+ */
+
+console.log("Hello, World!")
+
+console.log("\nDone.")
+
